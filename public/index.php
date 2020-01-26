fe646389f2a9b61f70ca0da355630569aeca903b
Pavel Leschev <isize1ce@gmail.com>
2020-01-26 10:46:27 +0500

    Add post endpoints

--- public/index.php
+++ public/index.php
@@ -1,19 +1,64 @@
 <?php
 
+use Post\PostFacade;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpNotFoundException;
 use Slim\Factory\AppFactory;
+use Slim\JsonResponse;
 use Slim\Psr7\Request;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/posts/{id:\d}', function (Request $request) {
+$app->get('/posts/{id:\d}', function (Request $request, PostFacade $postFacade) {
+    $id = (int)$request->getAttribute('id');
+    if ($id <= 0) {
+        throw new HttpBadRequestException($request);
+    }
+
+    $post = $postFacade->getById($id);
+    if ($post === null) {
+        throw new HttpNotFoundException($request);
+    }
+
+    return new JsonResponse([
+        'id' => $post->getId(),
+        'title' => $post->getTitle(),
+        'text' => $post->getText()
+    ]);
 });
 
-$app->post('/posts/{id:\d}', function (Request $request) {
+$app->post('/posts', function (Request $request, PostFacade $postFacade) {
 });
 
-$app->put('/posts/{id:\d}', function (Request $request) {
+$app->put('/posts/{id:\d}', function (Request $request, PostFacade $postFacade) {
+    $id = (int)$request->getAttribute('id');
+    if ($id <= 0) {
+        throw new HttpBadRequestException($request);
+    }
+
+    $post = $postFacade->getById($id);
+    if ($post === null) {
+        throw new HttpNotFoundException($request);
+    }
+
+    $body = $request->getParsedBody();
+
+    if (array_key_exists('title', $body)) {
+        $post->setTitle($body['title']);
+    }
+    if (array_key_exists('text', $body)) {
+        $post->setText($body['text']);
+    }
+
+    $postFacade->update($post);
+
+    return new JsonResponse([
+        'id' => $post->getId(),
+        'title' => $post->getTitle(),
+        'text' => $post->getText()
+    ]);
 });
 
 $app->run();
