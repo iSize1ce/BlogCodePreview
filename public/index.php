a5cd3c07f2da5094a7d0c58cd4a5436fcf7066a7
Pavel Leschev <isize1ce@gmail.com>
2020-01-26 10:34:15 +0500

    Add slim framework

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
