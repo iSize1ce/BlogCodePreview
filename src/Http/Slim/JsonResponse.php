8446f5edee04210ff64d1cf75de2a3bba4294349
Pavel Leschev <isize1ce@gmail.com>
2020-01-30 21:53:33 +0500

    Add empty json responses

--- src/Http/Slim/JsonResponse.php
+++ src/Http/Slim/JsonResponse.php
@@ -7,10 +7,15 @@ use Slim\Psr7\Response;
 
 class JsonResponse extends Response
 {
+    /**
+     * @param null|string|array $data
+     */
     public function __construct($data, int $status = StatusCodeInterface::STATUS_OK)
     {
         parent::__construct($status);
 
-        $this->getBody()->write(json_encode($data, JSON_THROW_ON_ERROR));
+        if ($data !== null) {
+            $this->getBody()->write(json_encode($data, JSON_THROW_ON_ERROR));
+        }
     }
 }
