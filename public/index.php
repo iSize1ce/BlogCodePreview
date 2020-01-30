204186f00d16ac0042a686969990f6560e0bfbdd
Pavel Leschev <isize1ce@gmail.com>
2020-01-30 21:36:14 +0500

    Add todo

--- public/index.php
+++ public/index.php
@@ -11,6 +11,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 (static function () {
     $app = AppFactory::create();
 
+    // @todo add DI container
+
     $app->group('/posts', static function (RouteCollectorProxy $postApp) {
         $postApp->post('/', CreatePostHandler::class);
 
